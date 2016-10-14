d7efdc55ceb73063a3fdde93e623c2b39f859719
Debanit Das <ddas@pivotal.io>
2016-10-14 10:15:48 -0400

    Add PATH to concourse startup script
    Signed-off-by: George Gelashvili <ggelashvili@pivotal.io>

--- concourse.sh
+++ concourse.sh
@@ -2,6 +2,8 @@
 CONCOURSE_PATH=$HOME/workspace/vagrant-concourse
 CONCOURSE_EXTERNAL_PORT=8000
 
+PATH=$PATH:/usr/local/bin
+
 if [ ! -d $CONCOURSE_PATH ]; then
   mkdir -p $CONCOURSE_PATH
 fi
@@ -14,5 +16,4 @@ if [ ! -f Vagrantfile ]; then
 config.vm.network "forwarded_port", guest: 8080, host: '"$CONCOURSE_EXTERNAL_PORT"'/' Vagrantfile
 fi
 
-vagrant up
-
+vagrant up 1> ~/workspace/macmini-autorun/stdout.log 2> ~/workspace/macmini-autorun/stderr.log
